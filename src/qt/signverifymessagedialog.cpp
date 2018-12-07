0d9ae7babc479dc73e29a08336b58c1a85c81688
pyritepirate <pyritepirate@protonmail.com>
2018-12-07 05:03:15 +0100

    Change Generic Address For PYE Address Pt.2 SMH

--- src/qt/signverifymessagedialog.cpp
+++ src/qt/signverifymessagedialog.cpp
@@ -24,10 +24,10 @@ SignVerifyMessageDialog::SignVerifyMessageDialog(QWidget *parent) :
 
 #if (QT_VERSION >= 0x040700)
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
-    ui->addressIn_SM->setPlaceholderText(tr("Enter a Pyrite address (e.g. B8gZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)"));
+    ui->addressIn_SM->setPlaceholderText(tr("Enter a Pyrite address (e.g. PQAywd4dL1MeABQvEdoJgo98oBiMZL23mt)"));
     ui->signatureOut_SM->setPlaceholderText(tr("Click \"Sign Message\" to generate signature"));
 
-    ui->addressIn_VM->setPlaceholderText(tr("Enter a Pyrite address (e.g. B8gZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)"));
+    ui->addressIn_VM->setPlaceholderText(tr("Enter a Pyrite address (e.g. PQAywd4dL1MeABQvEdoJgo98oBiMZL23mt)"));
     ui->signatureIn_VM->setPlaceholderText(tr("Enter Pyrite signature"));
 #endif
 
