9c22e0e1f57e594d1266db998b5e2eb10eb55e44
pyritepirate <pyritepirate@protonmail.com>
2019-01-25 15:58:04 +0100

    Do Not Set An Addr Time Penalty When A Peer Advertises Itself

--- src/addrman.cpp
+++ src/addrman.cpp
@@ -246,6 +246,11 @@ bool CAddrMan::Add_(const CAddress &addr, const CNetAddr& source, int64_t nTimeP
     int nId;
     CAddrInfo *pinfo = Find(addr, &nId);
 
+    // Do not set a penality for a source's self-announcement
+    if (addr == source) {
+        nTimePenalty = 0;
+    }
+
     if (pinfo)
     {
         // periodically update nTime
